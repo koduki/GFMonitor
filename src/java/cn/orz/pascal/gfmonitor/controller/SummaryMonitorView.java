a5821a5c964cb63f52eff2cd6793071f0cfcc6a9
koduki <pascalm3@chuuden-no-MacBook-Air.local>
2012-01-08 03:07:06 +0900

    fix get any bug .

--- src/java/cn/orz/pascal/gfmonitor/controller/SummaryMonitorView.java
+++ src/java/cn/orz/pascal/gfmonitor/controller/SummaryMonitorView.java
@@ -4,10 +4,12 @@
  */
 package cn.orz.pascal.gfmonitor.controller;
 
+import cn.orz.pascal.gfmonitor.model.dao.system.SystemLogFacade;
 import cn.orz.pascal.gfmonitor.model.monitor.SystemMonitor;
 import cn.orz.pascal.gfmonitor.models.entity.system.SystemLog;
 import java.util.ArrayList;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -18,28 +20,19 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class SummaryMonitorView {
-
+    @EJB
+    private SystemLogFacade systemLogFacade; 
     private List<SystemLog> systemLogs;
     
     /** Creates a new instance of SummaryMonitorView */
     public SummaryMonitorView() {
         this.systemLogs = new ArrayList<SystemLog>();
-        this.systemLogs.add(new SystemLog() {{
-            this.setHeapMemoryUsageUsed(100L);
-
-        }});
-        this.systemLogs.add(new SystemLog() {{
-            this.setHeapMemoryUsageUsed(200L);
-
-        }});
-        this.systemLogs.add(new SystemLog() {{
-            this.setHeapMemoryUsageUsed(300L);
-
-        }});
     }
 
     public List<SystemLog> getSystemLogs() {
-        return systemLogs;
+        this.systemLogs = systemLogFacade.findAll();
+        System.out.println("step1");
+        return this.systemLogs;
     }
 
     public void setSystemLogs(List<SystemLog> systemLogs) {
